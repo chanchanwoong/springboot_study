418de6bd654ac3029956d5c2811589fe7268377c
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-05-04 21:42:10 +0900

    Add: Entity

---
+++ src/main/java/com/spring/study/domain/Product.java
@@ -0,0 +1,32 @@
+package com.spring.study.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stock;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
