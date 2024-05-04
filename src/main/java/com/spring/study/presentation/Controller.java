ee3de437eed9fb894a27000f8454a4f04e1c77fb
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-05-04 20:33:58 +0900

    Add: POST API

--- src/main/java/com/spring/study/presentation/Controller.java
+++ src/main/java/com/spring/study/presentation/Controller.java
@@ -4,12 +4,14 @@ import com.spring.study.vo.MemberRequestVo;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/get-api")
+@RequestMapping("/api/v1")
 public class Controller {
 
     @GetMapping("/variable1/{variable}")
@@ -38,6 +40,22 @@ public class Controller {
 
     @GetMapping("/request3")
     public String getRequestParam3(MemberRequestVo memberRequestVo) {
+        return memberRequestVo.toString();
+    }
+
+    @PostMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping("/member2")
+    public String postMember2(@RequestBody MemberRequestVo memberRequestVo) {
         return memberRequestVo.toString();
     }
 }
